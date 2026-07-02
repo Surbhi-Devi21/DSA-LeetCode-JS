c885d15b1dc2cc6058f9e38fbf3fc76880daa422
Surbhi Devi <surbhimishra3135@gmail.com>
2026-07-03 01:13:05 +0530

    Time: 13 ms (73.79%), Space: 57.7 MB (15.71%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -4,38 +4,38 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-      nums.sort((a, b) => a - b);
-
-    let closest = nums[0] + nums[1] + nums[2];
-
-    for (let i = 0; i < nums.length - 2; i++) {
-
-        let left = i + 1;
-        let right = nums.length - 1;
-
-        while (left < right) {
-
-            let sum = nums[i] + nums[left] + nums[right];
-
-            // Update closest if current sum is nearer to target
-            if (Math.abs(target - sum) < Math.abs(target - closest)) {
-                closest = sum;
+  nums.sort((a,b) => a - b);
+  let max_diff = Infinity ;
+   let closest = null ;
+  for( let i = 0 ; i < nums.length - 2 ; i++){
+    let left = i + 1;
+    let right = nums.length - 1;
+    
+    while(left < right){
+        let sum = nums[i] + nums[left]+ nums[right];
+       
+        let diff = Math.abs( sum - target) ;
+        if(diff < max_diff) {
+        max_diff = diff ; 
+         closest = sum ;
+               
+        }
+        else {
+            if ( sum === target){
+                return sum
             }
 
-            // Exact answer mil gaya
-            if (sum === target) {
-                return sum;
+            else if ( sum > target){
+                right-- ;
             }
-            // Sum chhota hai → increase it
-            else if (sum < target) {
-                left++;
-            }
-            // Sum bada hai → decrease it
             else {
-                right--;
+                left++ ;
             }
         }
     }
+    
+  }
 
-    return closest;
+  return closest ;
+
 };
