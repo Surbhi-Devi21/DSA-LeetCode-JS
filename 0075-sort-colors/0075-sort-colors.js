f6732e15e1b48b31ed95660de33cb99e45ab3a9a
Surbhi Devi <surbhimishra3135@gmail.com>
2026-07-05 03:43:15 +0530

    Time: 0 ms (100%), Space: 53.3 MB (80.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,6 +3,30 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    nums.sort((a,b)=> a-b);
+    let start = 0 ;
+    let end = nums.length - 1;
+    let mid = 0;
+
+    while(mid <= end){
+        if(nums[mid] === 2){
+            let temp = nums[mid];
+            nums[mid] = nums[end];
+            nums[end] = temp ;
+            end--;  
+        }
+
+        else if(nums[mid] === 0) {
+            let temp = nums[mid];
+            nums[mid] = nums[start];
+            nums[start] = temp ;
+            start++ ;
+            mid++ ;
+        }
+
+        else {
+            mid++ ;
+        }
+        
+    }
     return nums;
 };
