035678abaf23f1fd5e07cc287b75ce4e8d0eed5b
Surbhi Devi <surbhimishra3135@gmail.com>
2026-08-04 23:57:14 +0530

    Time: 4 ms (40.94%), Space: 58.8 MB (32.28%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+
+    const set = new Set(nums);
+    const ans = [];
+
+    for (let i = min; i <= max; i++) {
+        if (!set.has(i)) {
+            ans.push(i);
+        }
+    }
+
+    return ans;
+};
