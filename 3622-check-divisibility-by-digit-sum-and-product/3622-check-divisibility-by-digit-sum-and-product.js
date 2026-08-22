04607d20bea76c692968b48ae6c202db1aa22040
Surbhi Devi <surbhimishra3135@gmail.com>
2026-08-22 23:57:08 +0530

    Time: 0 ms (100%), Space: 55.5 MB (21.74%) - LeetHub

---
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var checkDivisibility = function(n) {
+     let temp = n;
+    let sum = 0;
+    let product = 1;
+
+    while (temp > 0) {
+        const digit = temp % 10;
+
+        sum += digit;
+        product *= digit;
+
+        temp = Math.floor(temp / 10);
+    }
+
+    return n % (sum + product) === 0;
+
+};
