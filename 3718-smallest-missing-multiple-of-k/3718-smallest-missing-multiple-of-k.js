a2d0b19039f7472bf5699339ac2863826f14a8b4
Surbhi Devi <surbhimishra3135@gmail.com>
2026-08-25 23:51:21 +0530

    Time: 2 ms (23.53%), Space: 56.3 MB (47.06%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var missingMultiple = function(nums, k) {
+     const set = new Set(nums);
+
+    let multiple = k;
+
+    while (set.has(multiple)) {
+        multiple += k;
+    }
+
+    return multiple;
+
+};
